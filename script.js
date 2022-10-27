0329406e67ceccd824d760a700d80f7043484356
gsnorberto <sn_gabriel@outlook.com>
2022-10-27 08:10:54 -0300

    feat: Logar no chat e permanecer conectado

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 //Scroll to last message
 let lastMessage = document.querySelector('.msgs .msg:last-child');
 lastMessage.scrollIntoView();
+
+let userName = 'gsnorberto';
+
+// Join the chat
+axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: userName })
+    .then(()=>{console.log("Conectado...");})
+    .catch((e)=>{console.log("Error: ");})
+
+// Stay online
+let intervalStayOn = setInterval(stayOn, 5000);
+
+function stayOn () {
+    axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: userName })
+        .then(()=>{console.log("Conectado...");})
+        .catch((e)=>{console.log("Error: ");})
+}
+
