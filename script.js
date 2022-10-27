1d2fde94746e296f2aa86073ba5c08d893a890c7
gsnorberto <sn_gabriel@outlook.com>
2022-10-27 09:35:46 -0300

    feat: Busca e inserção de mensagens do servidor

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-//Scroll to last message
-let lastMessage = document.querySelector('.msgs .msg:last-child');
-lastMessage.scrollIntoView();
+const userName = 'gsnorberto';
+let allMessages = [];
 
-let userName = 'gsnorberto';
+
 
 // Join the chat
 axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: userName })
     .then(()=>{console.log("Conectado...");})
-    .catch((e)=>{console.log("Error: ");})
+    .catch((e) => {console.log("Error: ", e.response.status);})
 
-// Stay online
+
+// Keep User Online
 let intervalStayOn = setInterval(stayOn, 5000);
-
 function stayOn () {
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: userName })
-        .then(()=>{console.log("Conectado...");})
-        .catch((e)=>{console.log("Error: ");})
+        .then(() => {console.log("Conectado...");})
+        .catch((e) => {console.log("Error: ", e.response.status);})
+}
+
+//Fetch Message
+function fetchMessage () {
+    allMessages = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+        .then((response) => {
+            allMessages = response.data;
+            console.log(allMessages);
+            insertMessage();
+        })
+        .catch((e) => {console.log("Error: ", e.response.status);})
+}
+fetchMessage();
+
+// Insert Messages
+function insertMessage () {
+    for(let i = 0 ; i < allMessages.length ; i++){
+        document.querySelector('.msgs').innerHTML += `
+            <div class="msg">
+                <div class="send-time">
+                    ${allMessages[i].time}
+                </div>
+                <div class="text">
+                    <span>${allMessages[i].from}</span> ${allMessages[i].text}...
+                </div>
+            </div>
+        `
+    }
+    
+    //Scroll to last message
+    let lastMessage = document.querySelector('.msgs .msg:last-child');
+    lastMessage.scrollIntoView();
 }
 
