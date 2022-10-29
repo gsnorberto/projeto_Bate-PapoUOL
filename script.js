7021cff9a98dd6545cc179beda8bda24f60b7dfb
gsnorberto <sn_gabriel@outlook.com>
2022-10-29 11:21:10 -0300

    feat: Agora é possível ver os usuários que estão online

--- script.js
+++ script.js
@@ -2,28 +2,31 @@ let userName;
 let allMessages = [];
 let intervalStayOn;
 let intervalFetchMessages;
+let intervalOnlineUsers;
+let onlineUsers = [];
 
-function enterName(){
+function enterName() {
     userName = document.querySelector('.login-area input').value;
 
-    if(userName.trim() !== ''){
+    if (userName.trim() !== '') {
         joinChat();
     }
 }
-enterName();
-    
+
 // Join the chat
-function joinChat(){
+function joinChat() {
     axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: userName })
-        .then(()=>{
-            console.log("Conctado...");
-            document.querySelector('.login').style.display = 'none';
+        .then(() => {
+            //console.log("Conctado...");
+            document.querySelector('.login').style.display = 'none'; // hide login
             intervalStayOn = setInterval(stayOn, 5000); // Keep User Online
             fetchMessages();
             intervalFetchMessages = setInterval(fetchMessages, 3000); // Fetch messages on the server
+            checkOnlineUsers();
+            intervalOnlineUsers = setInterval(checkOnlineUsers, 10000);
         })
         .catch((e) => {
-            if(e.response.status === 400){
+            if (e.response.status === 400) {
                 document.querySelector('.login-area input').value = '';
                 alert('Nome de usuário já está em uso');
             }
@@ -32,28 +35,69 @@ function joinChat(){
 }
 
 // Keep User Online
-function stayOn () {
+function stayOn() {
     axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: userName })
-        .then(() => {console.log("Conectado...");})
-        .catch((e) => {console.log("Error: ", e.response.status);})
+        .then(() => { console.log("Conectado..."); })
+        .catch((e) => {
+            console.log("Error: ", e.response.status);
+            document.querySelector('.login').style.display = 'flex';
+        })
+}
+
+// check online userrs
+function checkOnlineUsers() {
+    axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+        .then((response) => {
+            onlineUsers = response.data;
+            console.log("onlineUsers: ", onlineUsers);
+            insertUsers();
+        })
+        .catch((e) => { console.log("Nenhum usuário online"); })
+}
+// Insert online users in the navBar
+function insertUsers() {
+    document.querySelector('.contacts').innerHTML = '';
+    document.querySelector('.contacts').innerHTML = `
+        <div class="contact">
+            <div>
+                <ion-icon name="people-sharp"></ion-icon>
+                <div class="contact-name">Todos</div>
+            </div>
+            <ion-icon class="chek-icon" name="checkmark-sharp"></ion-icon>
+        </div>
+    `
+    for (let i = 0; i < onlineUsers.length; i++) {
+        if (onlineUsers[i].name !== userName) {
+            document.querySelector('.contacts').innerHTML += `
+            <div class="contact">
+                <div>
+                    <ion-icon name="person-circle"></ion-icon>
+                    <div class="contact-name">${onlineUsers[i].name}</div>
+                </div>
+            </div>
+        `
+        }
+
+    }
+
 }
 
 //Fetch Message
-function fetchMessages () {
-    allMessages = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
+function fetchMessages() {
+    axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
         .then((response) => {
             allMessages = response.data;
             //console.log(allMessages);
             insertMessage();
         })
-        .catch((e) => {console.log("Error: ", e.response.status);})
+        .catch((e) => { console.log("Error: ", e.response.status); })
 }
 
 // Insert Messages  in chat
-function insertMessage () {
+function insertMessage() {
     document.querySelector('.msgs').innerHTML = '';
 
-    for(let i = 0 ; i < allMessages.length ; i++){
+    for (let i = 0; i < allMessages.length; i++) {
         document.querySelector('.msgs').innerHTML += `
             <div class="msg ${allMessages[i].type == 'status' ? 'status-msg' : ''} ${allMessages[i].from == userName ? 'reserved-msg' : ''}">
                 <div class="text">
@@ -62,17 +106,17 @@ function insertMessage () {
             </div>
         `
     }
-    
+
     //Scroll to last message
     let lastMessage = document.querySelector('.msgs .msg:last-child');
     lastMessage.scrollIntoView();
 }
 
 //Send Message
-function sendMessage () {
+function sendMessage() {
     let text = document.querySelector('.input-text').value;
 
-    if(text !== ''){
+    if (text !== '') {
         let message = {
             from: userName,
             to: 'Todos',
@@ -89,7 +133,7 @@ function sendMessage () {
                 alert('Você não está mais na sala!')
                 window.location.reload();
             })
-        
+
         //clear input text
         document.querySelector('.input-text').value = '';
     }
@@ -98,25 +142,25 @@ function sendMessage () {
 // Send with 'enter' key
 let inputLogin = document.querySelector('.login input');
 inputLogin.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13){
+    if (e.keyCode === 13) {
         enterName();
     }
 })
 let inputMsg = document.querySelector('.input-text');
 inputMsg.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13){
+    if (e.keyCode === 13) {
         sendMessage();
     }
 })
 
 // close navBar - click outside navBar
 document.querySelector('.navBar-area').addEventListener('click', (e) => {
-    if(!(document.querySelector('.navBar-contentArea').contains(e.target))){
+    if (!(document.querySelector('.navBar-contentArea').contains(e.target))) {
         document.querySelector('.navBar').style.display = 'none';
     }
 })
 
 //open navBar
-function openNavBar(){
+function openNavBar() {
     document.querySelector('.navBar').style.display = 'flex';
 }
